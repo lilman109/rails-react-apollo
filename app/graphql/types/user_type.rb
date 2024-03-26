fafe29c5f11ab512a003e3f0201a311bceba772a
lilman109 <akira.yoshidasjsu@gmail.com>
2024-03-26 22:22:43 +0900

    setup schema

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -5,7 +5,13 @@ module Types
     field :id, ID, null: false
     field :email, String
     field :name, String
+    field :posts, [Types::PostType], null: true
+    field :posts_count, Integer, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def posts_count
+      object.posts.size
+    end
   end
 end
